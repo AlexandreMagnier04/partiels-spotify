b8d117e6d946cd38bceaf2d5f5d9ff6fd84db6eb
AlexandreMagnier04 <159914932+AlexandreMagnier04@users.noreply.github.com>
2025-04-23 22:22:45 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['ressources/css/spotify-theme.css','resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/spotify-theme.css','resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
